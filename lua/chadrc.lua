23e2bb6b0e9021cbdeaccc207f6f7169b7fe9601
SolomonNova <993284vks@gmail.com>
2026-04-13 20:29:04 +0530

    bugs fixed

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -2,7 +2,7 @@
 local M = {}
 
 M.base46 = {
-    theme = "ashes",
+    theme = "catppuccin",
 }
 
 M.nvdash = {
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -11,17 +11,14 @@ return {
     end,
   },
 
-  -- ADD THIS SECTION BELOW:
   {
     "hrsh7th/nvim-cmp",
     opts = function()
       local cmp = require "cmp"
-      local conf = require "nvchad.configs.cmp" -- This pulls NvChad's default UI/settings
+      local conf = require "nvchad.configs.cmp"
 
-      -- Override the mappings
       conf.mapping["<Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
-          -- This forces the highlighted item to be SELECTED/CONFIRMED
           cmp.confirm { select = true }
         else
           fallback()
@@ -38,5 +35,80 @@ return {
 
       return conf
     end,
+  },
+
+  {
+    "mfussenegger/nvim-dap",
+    lazy = false,
+    config = function()
+      local dap = require "dap"
+
+      dap.adapters.gdb = {
+        type = "executable",
+        command = "gdb",
+        args = { "--quiet", "--interpreter=dap" },
+      }
+
+      dap.configurations.c = {
+        {
+          name = "Launch (GDB)",
+          type = "gdb",
+          request = "launch",
+          program = function()
+            return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+          end,
+          cwd = "${workspaceFolder}",
+          stopAtBeginningOfMainSubprogram = false,
+        },
+        {
+          name = "Attach to process",
+          type = "gdb",
+          request = "attach",
+          processId = require("dap.utils").pick_process,
+          cwd = "${workspaceFolder}",
+        },
+      }
+
+      dap.configurations.cpp = dap.configurations.c
+
+      vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc = "Toggle Breakpoint" })
+      vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "Continue" })
+      vim.keymap.set("n", "<leader>do", dap.step_over, { desc = "Step Over" })
+      vim.keymap.set("n", "<leader>di", dap.step_into, { desc = "Step Into" })
+      vim.keymap.set("n", "<leader>dO", dap.step_out, { desc = "Step Out" })
+      vim.keymap.set("n", "<leader>dq", dap.terminate, { desc = "Terminate" })
+    end,
+  },
+
+  {
+    "nvim-neotest/nvim-nio",
+  },
+
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+    config = function()
+      local dap = require "dap"
+      local dapui = require "dapui"
+
+      dapui.setup()
+
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+
+      vim.keymap.set("n", "<leader>du", dapui.toggle, { desc = "Toggle DAP UI" })
+    end,
+  },
+
+  {
+    "theHamsta/nvim-dap-virtual-text",
+    opts = {},
   },
 }
